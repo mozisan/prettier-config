afcb90358aa99bad43afa7938974be78f351abb3
mozisan <cc.sig@icloud.com>
2023-01-10 10:47:11 +0900

    Split config for typescript

--- src/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	plugins: [require.resolve("prettier-plugin-packagejson"), require.resolve("prettier-plugin-organize-imports")],
-	pluginSearchDirs: false,
-	printWidth: 120,
-	trailingComma: "all",
-	useTabs: true,
-};
--- src/react.js
+++
@@ -1,6 +0,0 @@
-const config = require("./main");
-
-module.exports = {
-	...config,
-	plugins: [...config.plugins, require.resolve("prettier-plugin-tailwindcss")],
-};
